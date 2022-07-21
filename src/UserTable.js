862727a5f7074f40e96ef181f1f01e8f780ad0f5
adarsh tripathi <adarshtripathi.2508@gmail.com>
2022-07-21 18:33:22 +0530

    update file edit

--- src/UserTable.js
+++ src/UserTable.js
@@ -1,5 +1,5 @@
-import { React, useState, useEffect } from 'react';
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import { React, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import axios from '../src/Axios/Axios';
 import TextField from '@mui/material/TextField';
@@ -8,31 +8,30 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import Box from '@mui/material/Box';
-import LinearProgress from '@mui/material/LinearProgress';
 
 
 const UserTable = ({ data, removeItem, setData }) => {
-   const navigate = useNavigate();
+    const navigate = useNavigate();
 
     const [open, setOpen] = useState(false);
-    const [persondata, setPersonData] = useState([]);
-   
+    const [persondata, setPersonData] = useState({});
+
 
 
     const handleClickOpen = (e) => {
-        setOpen(true);
+
         const dataId = e.target.dataset.userid;
-        
-       
+
+
         // axios.get(`users/${dataId}`).then((response) => {
         //     setPersonData(response.data);
         // })
         const filterData = data.filter((ele) => {
-            return ele.id == dataId;
+            return ele.id === dataId;
         })
         setPersonData(filterData[0]);
         // console.log(persondata)
+        setOpen(true);
     };
     const handleClose = () => {
         setOpen(false);
@@ -40,11 +39,18 @@ const UserTable = ({ data, removeItem, setData }) => {
 
     const editData = (e) => {
         e.preventDefault()
-       const getid = e.target.dataset.id;
-       console.log(getid);
-        axios.post(`users/${getid}`, persondata).then((response) => {
+        // debugger;
+        const userData = {
+            "first_name":persondata.first_name,
+            "last_name":persondata.last_name,
+            "email" : persondata.email
+        };
+
+        console.log(userData); console.log(persondata.id);
+        axios.post(`users/${persondata.id}`, userData).then((response) => {
             setPersonData(response.data);
-            // navigate('/');
+            setOpen(false);
+            navigate('/');
         })
     }
 
@@ -72,80 +78,7 @@ const UserTable = ({ data, removeItem, setData }) => {
                                 </td>
                                 <td><Link className="delete" to="" onClick={() => removeItem(ele.id)}>Delete</Link></td>
                             </tr>
-                            <div className="col-md-12">
-                            <Dialog open={open}>
-                                        <form id="my-form-id" onSubmit={editData} data-id={persondata.id}>
-                                            <DialogTitle>Update Blog</DialogTitle>
-                                            <DialogContent>
-                                                <DialogContentText>
-                                                    Edit User Information, Please enter Title and Content here.
-                                                </DialogContentText>
-                                                <TextField
-                                                    autoFocus
-                                                    margin="dense"
-                                                    name="last_name"
-                                                    label="Last Name"
-                                                    type="text"
-                                                    onChange={(e) => {
-                                                        const temp = Object.assign({}, data);
-                                                        temp.last_name = e.target.value;
-                                                        setPersonData(temp);
-                                                        console.log(persondata)
-                                                    }
 
-                                                    }
-                                                    value={persondata.last_name}
-                                                    fullWidth
-                                                />
-                                                <TextField
-                                                    autoFocus
-                                                    margin="dense"
-                                                    name="first_name"
-                                                    label="First Name"
-                                                    type="text"
-                                                    onChange={(e) => {
-                                                        const temp = Object.assign({}, data);
-                                                        temp.first_name = e.target.value;
-                                                        setPersonData(temp);
-                                                    }
-                                                    }
-                                                    value={persondata.first_name}
-                                                    fullWidth
-                                                />
-                                                <TextField
-                                                    autoFocus
-                                                    margin="dense"
-                                                    name="Email"
-                                                    label="Email"
-                                                    type="text"
-                                                    onChange={(e) => {
-                                                        const temp = Object.assign({}, data);
-                                                        temp.email = e.target.value;
-                                                        setPersonData(temp);
-                                                    }
-
-                                                    }
-                                                    value={persondata.email}
-                                                    fullWidth
-                                                />
-                                                <TextField
-                                                    autoFocus
-                                                    margin="dense"
-                                                    name="phone"
-                                                    label="Phone Number"
-                                                    type="text"
-
-                                                    // value={persondata.meta.phone_number}
-                                                    fullWidth
-                                                />
-                                            </DialogContent>
-                                            <DialogActions>
-                                                <Button onClick={handleClose}>Cancel</Button>
-                                                <Button type="submit">Submit</Button>
-                                            </DialogActions>
-                                        </form>
-                                    </Dialog>
-                            </div>
 
                         </>
                     )
@@ -153,6 +86,66 @@ const UserTable = ({ data, removeItem, setData }) => {
                 })
 
             }
+                <Dialog open={open}>
+                    <form id="my-form-id" onSubmit={editData} data-id={persondata.id}>
+                        <DialogTitle>Update Blog</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                Edit User Information, Please enter Title and Content here.
+                            </DialogContentText>
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                name="last_name"
+                                label="Last Name"
+                                type="text"
+                                onChange={(e) => {
+                                    setPersonData((prev) => ({ ...prev, [e.target.name] : e.target.value }))
+                                }}
+                                value={persondata.last_name}
+                                fullWidth
+                            />
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                name="first_name"
+                                label="First Name"
+                                type="text"
+                                onChange={(e) => {
+                                    setPersonData((prev) => ({ ...prev, [e.target.name] : e.target.value }))
+                                }}
+                                value={persondata.first_name}
+                                fullWidth
+                            />
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                name="email"
+                                label="Email"
+                                type="text"
+                                onChange={(e) => {
+                                    setPersonData((prev) => ({ ...prev, [e.target.name] : e.target.value }))
+                                }}
+                                value={persondata.email}
+                                fullWidth
+                            />
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                name="phone"
+                                label="Phone Number"
+                                type="text"
+
+                                // value={persondata.meta.phone_number}
+                                fullWidth
+                            />
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose}>Cancel</Button>
+                            <Button type="submit">Submit</Button>
+                        </DialogActions>
+                    </form>
+                </Dialog>
         </>
     )
 }
